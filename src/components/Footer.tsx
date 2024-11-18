e5c84af9a43e87cdb092fdd4343a35936fc46ca4
Tim <github@timgreen.ws>
2024-11-18 12:54:58 +1100

    removed unused vars

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => {
   return (
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import logo from "../assets/images/albert-logo.svg";
 
 const Header = () => {
